ab3a9fa577745d3515a5bc033438ebb88a5399e9
jessehartloff <hartloff@buffalo.edu>
2017-03-07 16:56:07 -0500

    initial commit

---
+++ src/AlgorithmsLecture2.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class AlgorithmsLecture2 {
+
+
+    // 5 points
+    static boolean Q1(ArrayList<String> input, String toFind) {
+
+        // return true if the input ArrayList contains a String with the same value as toFind, false otherwise
+        // Note: you must use .equals to compare Strings for equal values, not ==
+
+
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
